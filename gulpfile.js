2cf99e8b1d95a008d3a4d653d09b1b4bf605916e
Thomas Dimnet <Thomas.dimnet@gmail.com>
2017-02-04 13:07:45 +0100

    Add gulp-uglify & gulp-rename dependencies and create new script which minifies and renames app.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 'use strict';
 
 var gulp = require('gulp'),
-  concat = require('gulp-concat');
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify'),
+  rename = require('gulp-rename');
 
 gulp.task("concatScripts", function() {
   gulp.src([
@@ -12,5 +14,12 @@ gulp.task("concatScripts", function() {
     'js/foundation.reveal.js'
   ])
   .pipe(concat('app.js'))
+  .pipe(gulp.dest('js'));
+});
+
+gulp.task("minifyScripts", function() {
+  gulp.src("js/app.js")
+  .pipe(uglify())
+  .pipe(rename('app.min.js'))
   .pipe(gulp.dest('js'));
 });
